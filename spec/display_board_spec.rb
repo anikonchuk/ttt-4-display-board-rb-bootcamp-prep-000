83ba527e3cf5d6e0eb76a60ebcfb4b41e84a68a6
Andrew Nikonchuk <anikonch@gmail.com>
2018-05-03 13:38:41 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -182,9 +182,11 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
+      
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] # This is now correct
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
